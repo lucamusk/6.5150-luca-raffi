9db27dad74b0552fdafe523b7b332999483d9013
Luca Musk <luca@lucamusk.com>
2025-05-01 23:00:37 -0400

    Blocks and loop fusion largely done, though need testing for block sequence combos

--- opt-interface.scm
+++ opt-interface.scm
@@ -1,9 +1,9 @@
-#|
+
 (load "./sdf/manager/load.scm")
 (manage 'new 'term)
 (manage 'add 'design)
 (manage 'add 'unification)
-|#
+
 
 
 ;;; loop fusion, loop tiling, loop hoisting, constant folding, dead code elimination,
@@ -42,8 +42,8 @@
        (pp "cool"))))
 
 (run-matcher
- (match:compile-pattern `((? b) 3))
- `((1 (b 3)))
+ (match:compile-pattern `(pp "wow"))
+ `(pp "wow")
  match:bindings)
 
 (define (true-succ tree curr)
@@ -70,9 +70,28 @@
 (search-tree `((b 0 (1 2 0)) a (1 (b 3))) `((? b) 3) true-succ)
 
 ;;; Performs a deep copy of a given tree, with transformations
-;;; done on the first sublist beginning with an element matching
-;;; the given pattern
-(define (transform-tree tree pattern transformer)
+;;; done on the first or all elements matching the given
+;;; pattern
+(define (transform-tree tree pattern transformer all?)
+  (let ((pattern-compiled (match:compile-pattern pattern))
+        (matched? #f))
+    (define (traverse-tree current)
+      (if (and matched? (not all?))
+          current
+          (if (run-matcher pattern-compiled current match:bindings)
+              (begin
+		(set! matched? #t)
+                (transformer current))
+	      (if (pair? current)
+		  (let ((v (traverse-tree (car current)))) ;;; Dig down
+                    (cons v (traverse-tree (cdr current)))) ;;; Dig across
+		  current))))
+  (traverse-tree tree)))
+
+;;; Performs a deep copy of a given tree, with transformations
+;;; done on the first subtree beginning with an element that
+;;; matches the given pattern. Necessary for sequences to work
+(define (transform-subtree tree pattern transformer)
   (let ((pattern-compiled (match:compile-pattern pattern))
         (matched? #f))
     (define (traverse-tree current)
@@ -82,14 +101,14 @@
               current
               (if (run-matcher pattern-compiled (car current) match:bindings)
                   (begin
-                    (set! matched? #t)
-                    (transformer current))
+		    (set! matched? #t)
+		    (transformer current))
                   (let ((v (traverse-tree (car current)))) ;;; Dig down
                     (cons v (traverse-tree (cdr current)))))))) ;;; Dig across
     (traverse-tree tree)))
 
-(transform-tree `((b 0 (1 2 0)) a (1 (b 3) 3)) `((? b) 3) (lambda (t) 'cool))
-;;; -> ((b 0 (1 2 0)) a (1 1 3))
+(transform-tree `((b 0 (1 2 0)) a (1 (b 2) 3)) `((? b) 2) (lambda (t) 'cool) #f)
+;;; -> ((b 0 (1 2 0)) a (1 cool 3))
 
 ;;; Locations can either be across-wise (sequences) or depth-wise (blocks)
 (define (nth-location pattern amount)
@@ -104,46 +123,51 @@
 (define (nth-location-target loc)
   (cadr loc))
 
-(define (block pattern)
-  `(block pattern))
+(define (block block-pattern scope-index inner-pattern)
+  `(block ,block-pattern ,inner-pattern ,scope-index))
 (define (block? loc)
   (and (pair? loc)
-       (= (length loc) 2)
+       (= (length loc) 4)
        (equal? (car loc) 'block)))
+
+(define (block-scope-index loc)
+  (cadddr loc))
 
 (define (block-pattern loc)
   (cadr loc))
+
+(define (block-inner-pattern loc)
+  (caddr loc))
 
 ;;; Dummy predicate
 (define (ignore v)
   #t)
 
 ;;; Assumes no special handling for the matching code segment
-(define (generic-optimize-at code location optimizer)
-  (transform-tree code location optimizer))
+(define (generic-optimize-at code location optimizer all?)
+  (transform-tree code location optimizer all?))
 
 (define (debug-opt code)
   (pp code)
   code)
 
 (define optimize-at
-  (simple-generic-procedure `optimize-at 3 generic-optimize-at))
+  (simple-generic-procedure `optimize-at 4 generic-optimize-at))
 
 (define-generic-procedure-handler optimize-at
-  (match-args ignore nth-location? procedure?)
-  (lambda (code location optimizer)
-    (pp "opt at")
-    (pp code)
+  (match-args ignore nth-location? procedure? boolean?)
+  (lambda (code location optimizer all?)
     (let ((n (nth-location-count location))
           (target (nth-location-target location)))
       (if (= n 1)
-          (transform-tree code target optimizer)
-          (transform-tree code target
+          (transform-tree code target optimizer all?)
+          (transform-subtree code target
                           (lambda (c)
                             (cons (car c)
                                   (optimize-at (cdr c)
                                                (nth-location target (- n 1))
-                                               optimizer))))))))
+                                               optimizer
+					       all?))))))))
 
 (define test-sequences
   `((for i 0 10
@@ -154,15 +178,57 @@
       (pp "nice!"))))
 
 (optimize-at test-sequences (nth-location `(for i 0 10 (? block)) 2) (lambda (c)
-                                                                       (pp "optimizing")
-                                                                       (pp c)
-                                                                       (cons 'cool (cdr c))))
+                                                                       'cool)
+	     #f)
+;;; -> ((for i 0 10 (pp "wow")) cool (for i 0 10 (pp "nice!")))
+
+(define-generic-procedure-handler optimize-at
+  (match-args ignore block? procedure? boolean?)
+  (lambda (code location optimizer all?)
+    (let ((target-block (block-pattern location))
+	  (scope-index (block-scope-index location))
+	  (inner-pattern (block-inner-pattern location)))
+      (transform-tree code target-block
+		      (lambda (b)
+			(pp (list-ref b scope-index))
+			(let ((list-left
+			       (if (= scope-index 0)
+				   '()
+				   (sublist b 0 scope-index)))
+			      (list-right 
+			       (sublist b (+ scope-index 1) (length b))))
+			  (append list-left
+				  (cons 
+				   (optimize-at (list-ref b scope-index) inner-pattern optimizer all?)
+				   list-right))))
+				  
+		      #f))))
 
 (define top-level
   `(? code))
 
+(define test-blocks
+  `((for i 0 10 (
+		 (pp i)
+		 (pp "hey")))))
+
+(optimize-at test-blocks (block `(for i 0 10 (? block)) 4 `(pp i)) (lambda (c) 'cool) #f) 
+;;; -> ((for i 0 10 (cool (pp "hey"))))
+
+
+(define (rename var new)
+  (define optimizer (make-pattern-operator))
+  (attach-rule! optimizer
+		(rule `(,var)			 
+		      `,new))
+  optimizer)
+
+(optimize-at test-sequences 'i (rename 'i 'j) #t)
+;;; -> ((for j 0 10 (pp "wow")) (for j 0 10 (pp "hey")) (for j 0 10 (pp "nice!")))
+
 (define (loop-fuser loop-1 loop-2)
   (define optimizer (make-pattern-operator))
+  ;;; Where the bounds are equal
   (attach-rule! optimizer
                 (rule `(((?? a)
                          (for ,loop-1 (? bound-low) (? bound-high) (? body-1))
@@ -172,11 +238,47 @@
                       `(,@a
                         (for ,loop-1 ,bound-low ,bound-high
                           (,body-1
-                           ,body-2))
+                           ,(optimize-at body-2 loop-2 (rename loop-2 loop-1) #t)))
                         ,@b
                         ,@c)))
+  ;;; Where the bounds may not be equal (doesn't statically check if iterations are
+  ;;; the same, that's the programmer's burden
+  ;;; Numeric bounds
+  (attach-rule! optimizer
+		(rule `(((?? a)
+			 (for ,loop-1 (? bound-low-1, number?) (? bound-high-1) (? body-1))
+			 (?? b)
+			 (for ,loop-2 (? bound-low-2, number?) (? bound-high-2) (? body-2))
+			 (?? c)))
+		      `(,@a
+			(for ,loop-1 ,bound-low-1 ,bound-high-1
+			     (,body-1
+			      ,(optimize-at body-2 loop-2
+					    (rename loop-2 `(+ ,loop-1 ,(- bound-low-2 bound-low-1)))
+					    #t)))
+			,@b
+			,@c)))
+  
+  ;;; One or more symbolic bounds
+  (attach-rule! optimizer
+		(rule `(((?? a)
+			 (for ,loop-1 (? bound-low-1) (? bound-high-1) (? body-1))
+			 (?? b)
+			 (for ,loop-2 (? bound-low-2) (? bound-high-2) (? body-2))
+			 (?? c)))
+		      `(,@a
+			(for ,loop-1 ,bound-low-1 ,bound-high-1
+			     (,body-1
+			      ,(optimize-at body-2 loop-2
+					    (rename loop-2 `(+ ,loop-1 (- ,bound-low-2 ,bound-low-1)))
+					    #t)))
+			,@b
+			,@c)))
+			     
   optimizer)
 
+
+(optimize-at `(pp j j j) 
 ;;; Simple test for correctness
 (define lf-test-1
   '((for i 0 10
@@ -184,7 +286,7 @@
     (for j 0 10
       (pp "wow"))))
 
-(optimize-at lf-test-1 top-level (loop-fuser `i `j))
+(optimize-at lf-test-1 top-level (loop-fuser `i `j) #f)
 #|
 ((for i 0 10 ((pp "hi") (pp "wow"))))
 |#
@@ -200,11 +302,13 @@
     (for j 0 10
       (pp "neat"))))
 
-(optimize-at lf-test-2 top-level (loop-fuser `i `j))
+(optimize-at lf-test-2 top-level (loop-fuser `i `j) #f)
 #|
-((for i 0 10 ((pp "hi") (pp "wow")))
+(
+(for i 0 10 ((pp "hi") (pp "wow")))
 (for i 0 10 (pp "cool"))
-(for j 0 10 (pp "neat")))
+(for j 0 10 (pp "neat"))
+)
 |#
 
 ;;; Doesn't fuse across scopes
@@ -215,54 +319,37 @@
       (for j 0 10
         (pp "wow")))))
 
-(optimize-at lf-test-3 top-level (loop-fuser 'i 'j))
+(optimize-at lf-test-3 top-level (loop-fuser 'i 'j) #f)
 ;;; -> No applicable operations: (((for i 0 10 (pp "hi")) (for k 0 10 (for j 0 10 (pp "wow")))))
 
-;;; Doesn't fuse if the bounds are different
+;;; Fuses if different bounds
 (define lf-test-4
   '((for i 0 15
-      (pp "hi"))
-    (for k 0 10
-      (pp "wow"))))
+      (pp i))
+    (for k 15 30
+      (pp k))))
 
-(optimize-at lf-test-4 top-level (loop-fuser 'i 'k))
-;;; -> No applicable operations: (((for i 0 15 (pp "hi")) (for k 0 10 (pp "wow"))))
+(optimize-at lf-test-4 top-level (loop-fuser 'i 'k) #f)
+;;; -> ((for i 0 15 ((pp i) (pp (+ i 15)))))
 
-(define optimizer (make-pattern-operator))
+;;; Fuses if different variable bounds
+(define lf-test-5
+  '((for i 10 15
+      (pp i))
+    (for k x y
+      (pp k))))
 
-(attach-rule! optimizer
-              (rule `((begin
-                        ((?? a)
-                         (for i (? bound_low) (? bound_high) (? body_1))
-                         (?? b)
-                         (for j (? bound_low) (? bound_high) (? body_2))
-                         (?? c))))
-                    `(begin
-                       (,@a
-                        (for i ,bound_low ,bound_high
-                          (begin ,body_1 ,body_2))
-                        ,@b
-                        ,@c))))
+(optimize-at lf-test-5 top-level (loop-fuser 'i 'k) #f)
+;;; -> ((for i 0 15 ((pp i) (pp (+ i (- x 10))))))
 
+;;; Tests if we change the loop variables
+(define lf-test-6
+  `(
+    (for i 0 10
+	 (pp i))
+    (for j 0 10
+	 (pp j j j))))
 
-(define test1
-  `(begin
-     ((for i 0 x (pp "wow"))
-      (for j 0 x (pp "cool!")))))
+(optimize-at lf-test-6 top-level (loop-fuser 'i 'j) #f)
+;;; -> ((for i 0 10 ((pp i) (pp i i i))))
 
-(optimizer test1); -> (begin ((for i 0 x (begin (pp "wow") (pp "cool!")))))
-
-(define test2
-  `(begin
-     ((for i 0 10 (pp "wow"))
-      (for j 0 10 (pp "cool")))))
-
-(optimizer test2); -> (begin ((for i 0 10 (begin (pp "wow") (pp "cool!")))))
-
-(define test3
-  `(begin
-     ((for i 0 10 (pp "wow"))
-      (for i 0 10 (pp "cool"))
-      (for j 0 10 (pp "crazy")))))
-
-(optimizer test3)
